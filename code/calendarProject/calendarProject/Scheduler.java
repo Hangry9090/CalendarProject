abba0b939a9aa8c336f2591f0d19795e6dcad7be
ASisouphone <sisouphone.alan@gmail.com>
2018-01-26 11:52:43 -0500

    Added Jsoup API and makes it extract schedule from HTML

--- code/calendarProject/calendarProject/Scheduler.java
+++ code/calendarProject/calendarProject/Scheduler.java
@@ -4,8 +4,15 @@
 package calendarProject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * @author jacobwalton
@@ -49,16 +56,50 @@ public class Scheduler {
 			}
 	}
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		Scheduler schedule = new Scheduler();
 
-		ArrayList<String> testList = schedule.readFile("sched.html");
+//		ArrayList<String> testList = schedule.readFile("sched.html");
+//		
+//		int count = 0;
+//		for (String str : testList){
+//			System.out.println(count + ": " + str);
+//			count++;
+//		}
 		
-		int count = 0;
-		for (String str : testList){
-			System.out.println(count + ": " + str);
-			count++;
+		
+		ArrayList<String> mySchedule = new ArrayList<String>();
+		
+		
+		File input = new File("sched.html");
+	
+		
+		// Creates the schedule html into a Jsoup document object
+		Document doc = Jsoup.parse(input, "UTF-8");
+		
+		// All elements inside the datadisplaytable are stored in a Jsoup elements object
+		Elements schedTable = doc.getElementsByClass("datadisplaytable");
+				
+		// It will now separate the rows and columns of the table
+		Elements rows = schedTable.select("tr");
+		
+		for (Element row: rows) {
+			
+			Elements cells=row.select("td");
+			for (Element cell: cells) {
+				if (!cell.text().isEmpty()) {
+				//System.out.println(cell.text());
+					mySchedule.add(cell.text());
+				
+				}
+			}
+			
 		}
+		
+		for (String s: mySchedule) {
+			System.out.println(s);
+		}
+		
 	}
 
 }
