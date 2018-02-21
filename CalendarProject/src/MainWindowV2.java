710801c196fd6432fc4acefbb79f43002cc9e453
Marshal Brummel <mbrummel14@gmail.com>
2018-02-21 10:28:04 -0500

    Added comments

--- CalendarProject/src/MainWindowV2.java
+++ CalendarProject/src/MainWindowV2.java
@@ -23,11 +23,11 @@ public class MainWindowV2 {
 	private Text messageTextField;
 	private Scheduler userSchedule = new Scheduler();
 
-	/**
+	/**********************************************************************************
 	 * Launch the application.
 	 * 
 	 * @param args
-	 */
+	 *********************************************************************************/
 	public static void main(String[] args) {
 		try {
 			// Set cross-platform Java L&F (also called "Metal")
@@ -37,6 +37,7 @@ public class MainWindowV2 {
 			System.out.println("Problem setting LAF.");
 		}
 		try {
+			//Try to open the window
 			MainWindowV2 window = new MainWindowV2();
 			window.open();
 		} catch (Exception e) {
@@ -44,9 +45,9 @@ public class MainWindowV2 {
 		}
 	}
 
-	/**
+	/**********************************************************************************
 	 * Open the window.
-	 */
+	 *********************************************************************************/
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
@@ -59,21 +60,26 @@ public class MainWindowV2 {
 		}
 	}
 
-	/**
+	/********************************************************************************
 	 * Create contents of the window.
-	 */
+	 *******************************************************************************/
 	protected void createContents() {
+		
+		//Create the window
 		shell = new Shell();
 		shell.setSize(840, 580);
 		shell.setText("GVSU Scheduler");
 
+		//Create the container inside the window
 		Composite composite = new Composite(shell, SWT.NONE);
 		composite.setBounds(10, 10, 802, 513);
 		composite.setLayout(null);
 
+		//Add a tab-bar to container
 		TabFolder tabFolder = new TabFolder(composite, SWT.NONE);
 		tabFolder.setBounds(0, 0, 801, 511);
 
+		//Create home tab
 		TabItem homeTab = new TabItem(tabFolder, SWT.NONE);
 		homeTab.setText("Home");
 
@@ -81,15 +87,17 @@ public class MainWindowV2 {
 		homeTab.setControl(composite_1);
 		composite_1.setLayout(null);
 
+		//Adding input button
 		Button inputButton = new Button(composite_1, SWT.NONE);
 		inputButton.setBounds(129, 355, 243, 76);
 		inputButton.setText("Input");
 
+		//Action Listener for input button
 		inputButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
 
-				// code for filedialog found here:
+				// Code to open file choser found here:
 				// https://stackoverflow.com/questions/5703825/does-swing-support-windows-7-style-file-choosers
 				FileDialog dialog = new FileDialog(shell, SWT.OPEN);
 				String[] filterNames = new String[] {"HTML (.html)"};
@@ -104,6 +112,7 @@ public class MainWindowV2 {
 				File selectedFile = new File(path, fileName);
 				fileName = selectedFile.getAbsolutePath();
 
+				//Try to open the file in the scheduler object
 				try {
 					System.out.println(fileName);
 					userSchedule.inputFile(fileName);
@@ -121,46 +130,53 @@ public class MainWindowV2 {
 			}
 		});
 
+		//Create the output button
 		Button outputButton = new Button(composite_1, SWT.NONE);
 		outputButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
+				
+				//Set the file explorer filter to only show ICS files
 				FileDialog dialog = new FileDialog(shell, SWT.SAVE);
 				dialog.setFilterNames(new String[] {"ICS Files (.ics)"});
 				dialog.setFilterExtensions(new String[] {"*.ics"});
-				//System.out.println("Save to: " + dialog.open());
+				
+				//Open the file chooser
 				String savePath = dialog.open();
 				System.out.println("Save path: " + savePath);
+				
+				//Try to save the file using the scheduler object
 				try {
 					userSchedule.outputFile(savePath);
 				} catch (IOException e1) {
-					// TODO Auto-generated catch block
 					//file could not be opened
 					e1.printStackTrace();
 				} catch (ParseException e1) {
-					// TODO Auto-generated catch block
 					//file could not be parsed
 					e1.printStackTrace();
 				} catch (NoSuchFieldException e1) {
-					// TODO Auto-generated catch block
-					//e1.printStackTrace();
 					//no file loaded yet
+					e1.printStackTrace();
 				}
 			}
 		});
+		
+		//Set location and label of output
 		outputButton.setBounds(426, 355, 243, 76);
 		outputButton.setText("Output");
 
+		//Creates the text field above the buttons
 		messageTextField = new Text(composite_1, SWT.BORDER);
 		messageTextField.setText("Welcome!");
 		messageTextField.setBounds(129, 52, 540, 273);
 
+		//Creates instructions tab
 		TabItem instructionsTab = new TabItem(tabFolder, SWT.NONE);
 		instructionsTab.setText("Instructions");
 
+		//Creates container for the instructions tab
 		Composite composite_2 = new Composite(tabFolder, SWT.NONE);
 		instructionsTab.setControl(composite_2);
 
 	}
-
 }
