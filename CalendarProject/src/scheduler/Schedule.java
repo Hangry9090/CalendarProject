2b39d692860329e1f6e5d50ba56d4ece4d897b10
Jacob Walton <waltojac@mail.gvsu.edu>
2018-04-15 11:16:57 -0400

    Created Schedule class. Can't do much more until I know how to display it.

---
+++ CalendarProject/src/scheduler/Schedule.java
@@ -0,0 +1,24 @@
+package scheduler;
+
+import java.util.*;
+
+/**
+ * Title: Schedule
+ * Description: Class to hold information about a schedule. Main data 
+ * structure is an ArrayList of Courses.
+ * 
+ * @author Jacob Walton
+ *
+ **/
+public class Schedule {
+	/*
+	 * courseList - Main data structure to store a bunch of courses.
+	 */
+	private ArrayList<Course> courseList = new ArrayList<Course>();
+	
+	/*
+	 * Constructor for the class.
+	 */
+	public Schedule() {
+	}
+}
